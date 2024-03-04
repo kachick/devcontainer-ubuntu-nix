53e5ef58a4c03343d5d315a8f973cee5b1aadd23
Kenichi Kamiya <kachick1@gmail.com>
2024-03-04 11:50:44 +0900

    Update flake style

--- README.md
+++ README.md
@@ -4,8 +4,7 @@ Now implemented only for vscode
 
 ## Motivation
 
-- nixos/nix is not NixOS, but does not have some tools as `groupadd`
-  \
+- nixos/nix is not NixOS, but does not have some tools as `groupadd`\
   ref: <https://stackoverflow.com/questions/75653182/why-do-some-official-nix-docker-containers-not-have-the-nixos-rebuild-command>
 - Personally prefer ubuntu rather than [alpine](https://hub.docker.com/r/nixos/nix) for daily use
 - Snapshot that I wanted to know this [when addressed to create Nix devcontainer](https://github.com/kachick/wait-other-jobs/pull/517)
--- flake.nix
+++ flake.nix
@@ -1,6 +1,11 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/21061e4e4a0f46a6f487c7583be916e4e031ee4e";
+    # Candidate channels
+    #   - https://github.com/kachick/anylang-template/issues/17
+    #   - https://discourse.nixos.org/t/differences-between-nix-channels/13998
+    # How to update the revision
+    #   - `nix flake update --commit-lock-file` # https://nixos.org/manual/nix/stable/command-ref/new-cli/nix3-flake-update.html
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -13,11 +18,16 @@
         devShells.default = with pkgs;
           mkShell {
             buildInputs = [
-              dprint
-              go-task
+              # https://github.com/NixOS/nix/issues/730#issuecomment-162323824
+              # https://github.com/kachick/dotfiles/pull/228
+              bashInteractive
+
               nil
               nixpkgs-fmt
+              go-task
+              dprint
               typos
+
               hadolint
               coreutils
             ];
